63831f8e26aed172164336e8378b871f853da177
Gabriel <gabriel688466@gmail.com>
2018-06-03 14:28:35 -0600

    Please enter the commit message for your changes. Lines starting

---
+++ age_restriction.py
@@ -0,0 +1,9 @@
+name = input("What's your name?")
+
+age = int(input("How old are you?"))
+
+if age < 18:
+    print(f"Sorry {name} you are not of age to continue.")
+
+else:
+    print(f"Welcome in {name}!")
